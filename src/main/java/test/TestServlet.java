51c333d628136b0fe1726170fdf86dad43f5b07e
niya <niya@niya.local>
2019-08-10 11:34:40 +0900

    jspの呼び出しの追加

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -1,8 +1,9 @@
 package test;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+// import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TestServlet extends HttpServlet {
 
+
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html; charset=UTF-8");
+        
+        String view_path = "/WEB-INF/views/index.jsp";
 
-        PrintWriter out = resp.getWriter();
-        out.println("<h1>Hello Jetty World!</h1>");
+        RequestDispatcher dispatcher = req.getRequestDispatcher(view_path);
+
+        dispatcher.forward(req, resp);
     }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html ; characterset=UTF-8" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>To-Do-List</title>
+    </head>
+    <body>
+        <%= new java.util.Date() %>
+    </body>
+</html>
